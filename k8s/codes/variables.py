311f04cd45f17dea6cabc9232e2fdd99e90831b7
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-08 09:00:47 -0300

    updating

--- k8s/codes/variables.py
+++ k8s/codes/variables.py
@@ -1,2 +1,2 @@
-PATH_SOURCE = 's3://landing-zone-vini-etl-aws/data/{file}.csv'
+PATH_SOURCE = 's3://landing-zone-vini-etl-aws/data/ml-25m/{file}.csv'
 PATH_TARGET = 's3://processing-zone-vini-etl-aws/processing/{file}'
