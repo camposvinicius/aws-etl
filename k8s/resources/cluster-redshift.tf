901a2bce74ac61a1ff1899990c0163b5efed4511
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-11 18:34:22 -0300

    updating

--- k8s/resources/cluster-redshift.tf
+++ k8s/resources/cluster-redshift.tf
@@ -1,3 +1,125 @@
+resource "aws_vpc" "redshift_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "redshift-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "redshift_vpc_gw" {
+  vpc_id = aws_vpc.redshift_vpc.id
+
+  depends_on = [
+    aws_vpc.redshift_vpc
+  ]
+}
+
+resource "aws_default_security_group" "redshift_security_group" {
+  vpc_id = aws_vpc.redshift_vpc.id
+
+  ingress {
+    from_port   = 5439
+    to_port     = 5439
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "redshift-sg"
+  }
+
+  depends_on = [
+    aws_vpc.redshift_vpc
+  ]
+}
+
+resource "aws_subnet" "redshift_subnet_1" {
+  vpc_id                  = aws_vpc.redshift_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "redshift-subnet-1"
+  }
+
+  depends_on = [
+    aws_vpc.redshift_vpc
+  ]
+}
+
+resource "aws_subnet" "redshift_subnet_2" {
+  vpc_id                  = aws_vpc.redshift_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "redshift-subnet-2"
+  }
+
+  depends_on = [
+    aws_vpc.redshift_vpc
+  ]
+}
+
+resource "aws_redshift_subnet_group" "redshift_subnet_group" {
+  name = "redshift-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.redshift_subnet_1.id,
+    aws_subnet.redshift_subnet_2.id
+  ]
+
+  tags = {
+    environment = "vini-etl-aws"
+    Name        = "redshift-subnet-group"
+  }
+}
+
+
+resource "aws_iam_role_policy" "s3_full_access_policy" {
+  name = "redshift_s3_policy"
+
+  role   = aws_iam_role.redshift_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+}
+  EOF
+}
+
+resource "aws_iam_role" "redshift_role" {
+  name               = "redshift_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "redshift.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+  EOF
+
+  tags = {
+    tag-key = "redshift-role"
+  }
+}
+
 resource "aws_redshift_cluster" "default" {
   cluster_identifier  = "tf-redshift-cluster"
   database_name       = "etlvini"
@@ -7,49 +129,12 @@ resource "aws_redshift_cluster" "default" {
   cluster_type        = "single-node"
   skip_final_snapshot = true
   publicly_accessible = true
-  iam_roles           = ["role_redshift"]
+  iam_roles           = ["${aws_iam_role.redshift_role.arn}"]
 
   depends_on = [
-    aws_iam_role.role_redshift
+    aws_vpc.redshift_vpc,
+    aws_default_security_group.redshift_security_group,
+    aws_redshift_subnet_group.redshift_subnet_group,
+    aws_iam_role.redshift_role
   ]
-}
-
-resource "aws_iam_role_policy" "redshift_policy" {
-  name = "redshift_policy"
-  role = aws_iam_role.role_redshift.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:Get*",
-          "s3:List*"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  depends_on = [
-    aws_iam_role.role_redshift
-  ]
-}
-resource "aws_iam_role" "role_redshift" {
-
-  name = "role_redshift"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "redshift.amazonaws.com"
-        }
-      },
-    ]
-  })
 }
