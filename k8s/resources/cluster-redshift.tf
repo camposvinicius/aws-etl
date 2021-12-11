a1d3fb170d066725352a94f71c7758237af7a7cc
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-11 16:07:36 -0300

    Updating

--- k8s/resources/cluster-redshift.tf
+++ k8s/resources/cluster-redshift.tf
@@ -7,4 +7,31 @@ resource "aws_redshift_cluster" "default" {
   cluster_type        = "single-node"
   skip_final_snapshot = true
   publicly_accessible = true
+  iam_roles           = ["role_redshift"]
+
+  depends_on = [
+    aws_iam_role.role_redshift
+  ]
+}
+
+resource "aws_iam_role" "role_redshift" {
+  name = "role_redshift"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:Get*",
+          "s3:List*",
+          "sts:AssumeRole",
+        ]
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "redshift.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
