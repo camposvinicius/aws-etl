608032f69e03923961aa2072a35b54712eb00b99
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-13 20:23:00 -0300

    Updating

--- k8s/resources/cluster-redshift.tf
+++ k8s/resources/cluster-redshift.tf
@@ -20,7 +20,7 @@ resource "aws_default_security_group" "redshift_security_group" {
 
   ingress {
     from_port = 0
-    to_port   = 5439
+    to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
