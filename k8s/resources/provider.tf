c48b36406bf82e68ca73c642f30af116b64fe4d1
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-14 14:08:25 -0300

    updating

--- k8s/resources/provider.tf
+++ k8s/resources/provider.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  config_path = "/tmp/kubeconfig"
+  #host                   = data.aws_eks_cluster.cluster.endpoint
+  #cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  #token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
