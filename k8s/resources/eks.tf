d2a414cb7af9513be8aee08ffecaef7275e00763
camposvinicius <viniciuscampos199519@gmail.com>
2021-12-14 13:02:22 -0300

    updating

--- k8s/resources/eks.tf
+++ k8s/resources/eks.tf
@@ -17,15 +17,15 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.xlarge"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      instance_type                 = "t2.large"
       asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
     },
   ]
 }
