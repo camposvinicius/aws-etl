08a8cc7a21ee391975e35f84067672d881ec6099
camposvinicius <viniciuscampos199519@gmail.com>
2022-01-06 09:56:13 -0300

    updating

--- k8s/resources/backend.tf
+++ k8s/resources/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-vini-campos-etl-aws"
+    bucket = "tfstate-vini-campos-etl-aws-poc"
     key    = "terraform/tfstate"
     region = "us-east-1"
   }
