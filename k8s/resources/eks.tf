a2724104784fab0f9bf84e6df6dd9ca5cb21fa43
camposvinicius <viniciuscampos199519@gmail.com>
2022-01-11 17:22:50 -0300

    updating

--- k8s/resources/eks.tf
+++ k8s/resources/eks.tf
@@ -16,8 +16,8 @@ module "eks" {
     Vini = "ETL-AWS"
   }
 
-  vpc_id           = module.vpc.vpc_id
-  subnet_ids       = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
