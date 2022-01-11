a8cd564b6286d94bb5edcb3e5acc967044266b4d
camposvinicius <viniciuscampos199519@gmail.com>
2022-01-11 17:56:28 -0300

    updating

--- k8s/resources/eks.tf
+++ k8s/resources/eks.tf
@@ -7,6 +7,26 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+
+resource "aws_security_group" "additional" {
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+
+  tags = {
+    Vini = "ETL-AWS"
+  }
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
