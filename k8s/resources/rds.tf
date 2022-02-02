eb9cfad00ef23d0d84b46de3084677d942ecf483
camposvinicius <viniciuscampos199519@gmail.com>
2022-02-02 15:03:44 -0300

    updating

--- k8s/resources/rds.tf
+++ k8s/resources/rds.tf
@@ -9,30 +9,26 @@ resource "aws_db_instance" "vinipostgresql-instance" {
   publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.vinipostgresql.id]
   username               = var.postgres_user
-  password               = var.postgres_pass
+  password               = var.postgres_user
 
   tags = {
     tag-key = "vini-cluster-postgres-etl-aws"
   }
 }
 
-resource "aws_vpc" "rds_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "rds-vpc"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_security_group" "vinipostgresql" {
-  vpc_id = aws_vpc.rds_vpc.id
+  vpc_id = data.aws_vpc.default.id
   name   = "vinipostgresql"
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
