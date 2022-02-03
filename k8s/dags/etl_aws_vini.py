a8a4f41a8fb66c348ce8972f6364bc9b7252c996
camposvinicius <viniciuscampos199519@gmail.com>
2022-02-03 10:08:10 -0300

    updating

--- k8s/dags/etl_aws_vini.py
+++ k8s/dags/etl_aws_vini.py
@@ -86,22 +86,22 @@ EMR_CONFIG = {
                 'Name': 'MASTER_NODES',
                 'Market': 'ON_DEMAND',
                 'InstanceRole': 'MASTER',
-                'InstanceType': "m1.medium",
+                'InstanceType': "m1.small",
                 'InstanceCount': 1,
             },
             {
                 "Name": "CORE_NODES",
                 "Market": "ON_DEMAND",
                 "InstanceRole": "CORE",
-                "InstanceType": "m1.medium",
+                "InstanceType": "m1.small",
                 "InstanceCount": 1,
             },
             {
                 "Name": "TASK_NODES",
                 "Market": "SPOT",
-                "BidPrice": "0.012",
+                "BidPrice": "0.006",
                 "InstanceRole": "TASK",
-                "InstanceType": "m1.medium",
+                "InstanceType": "m1.small",
                 "InstanceCount": 1,
                 "AutoScalingPolicy":
                     {
--- k8s/resources/eks.tf
+++ k8s/resources/eks.tf
@@ -18,13 +18,13 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.xlarge"
+      instance_type                 = "r5.xlarge"
       asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.xlarge"
+      instance_type                 = "t2.small"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
